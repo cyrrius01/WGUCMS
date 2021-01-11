7074d2a571ef540267360dd2f953c29571207bca
Keith A Graham <Keith A Graham@DESKTOP-NN9ATKR>
2021-01-11 09:45:51 -0600

    Initial commit

--- src/controller/NewOrExistingBoxController.java
+++ src/controller/NewOrExistingBoxController.java
@@ -1,13 +1,18 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -38,15 +43,48 @@ public class NewOrExistingBoxController implements Initializable {
     }    
 
     @FXML
-    private void onNewCustomer(ActionEvent event) {
+    private void onNewCustomer(ActionEvent event) throws IOException {
+        
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/NewAppointmentNewCustomer.fxml"));
+
+        Stage newStage = new Stage();
+        newStage.setTitle(null);
+        Scene scene = new Scene(root);
+        newStage.setScene(scene);
+        newStage.show();
+        
     }
 
     @FXML
-    private void onExistingCustomer(ActionEvent event) {
+    private void onExistingCustomer(ActionEvent event) throws IOException {
+        
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/NewAppointmentExistingCustomer.fxml"));
+
+        Stage newStage = new Stage();
+        newStage.setTitle(null);
+        Scene scene = new Scene(root);
+        newStage.setScene(scene);
+        newStage.show();
+        
     }
 
     @FXML
-    private void onCancelCustomer(ActionEvent event) {
+    private void onCancelCustomer(ActionEvent event) throws IOException {
+        
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainScreen.fxml"));
+
+        Stage newStage = new Stage();
+        newStage.setTitle(null);
+        Scene scene = new Scene(root);
+        newStage.setScene(scene);
+        newStage.show();
+        
     }
     
 }
